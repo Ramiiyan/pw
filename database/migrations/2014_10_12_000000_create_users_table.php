7bdf81f8ace8ba1455f1b8206d63e6428ec6376a
Tharinda Rordigo <tharindarodrigo@gmail.com>
2021-06-07 17:47:32 +0530

    Removed the email field from the users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,8 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->nullable()->default(null);
-            $table->text('comment');
+//            $table->string('email')->nullable()->default(null);
+            $table->text('comments');
 //            $table->timestamp('email_verified_at')->nullable();
 //            $table->string('password');
 //            $table->rememberToken();
